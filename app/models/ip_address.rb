b9ecc44226ba71c420ff8b3f1b8fdf4e39745264
atton <e115763@gmail.com>
2015-10-19 18:12:57 +0900

    Add validation to IpAddress

--- app/models/ip_address.rb
+++ app/models/ip_address.rb
@@ -1,2 +1,7 @@
 class IpAddress < ActiveRecord::Base
+  validates_presence_of   :mac_address, :address
+  validates_uniqueness_of :mac_address, :address
+
+  validates_inclusion_of  :address,     in:   0..254
+  validates_format_of     :mac_address, with: /\A([0-9a-f]{2}:){5}([0-9a-f]{2})\z/
 end
